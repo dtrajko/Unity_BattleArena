9e68eebdb3ed135576531db4b62a2bbeae35cdb7
Dejan Trajkovic <dtrajko@gmail.com>
2019-06-22 01:52:04 +0200

    Section 27: Input - 202. Animation Movement

--- Assets/Project/Scripts/Player.cs
+++ Assets/Project/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
