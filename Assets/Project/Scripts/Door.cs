aea52c987616bfd2296e6863041976f6f83d29b9
Dejan Trajkovic <dtrajko@gmail.com>
2019-06-29 01:54:03 +0200

    Section 28: Battle Arena - 216. Opening Door

---
+++ Assets/Project/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private bool opensInwards;
+    [SerializeField] private float openingSpeed = 1.0f;
+
+    private bool isOpen;
+    private float targetAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Interact();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion smoothRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, targetAngle, 0), openingSpeed * Time.deltaTime);
+        // transform.localEulerAngles.y, targetAngle, openingSpeed * Time.deltaTime);
+        transform.localRotation = smoothRotation;
+    }
+
+    public void Interact() {
+        isOpen = !isOpen;
+        if (isOpen)
+        {
+            targetAngle = 90.0f * (opensInwards ? 1 : -1);
+        }
+        else {
+            targetAngle = 0;
+        }
+    }
+}
