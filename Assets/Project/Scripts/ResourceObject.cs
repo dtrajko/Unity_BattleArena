7a943631d8a04d260342dc04d7d775fa584c06d0
Dejan Trajkovic <dtrajko@gmail.com>
2019-07-02 23:31:01 +0200

    Section 28: Battle Arena - 234. Smooth Collection Animations

--- Assets/Project/Scripts/ResourceObject.cs
+++ Assets/Project/Scripts/ResourceObject.cs
@@ -6,24 +6,35 @@ public class ResourceObject : MonoBehaviour
 {
     [SerializeField] private int resourceAmount;
     [SerializeField] private int amountOfHits;
+    [SerializeField] private float hitScale = 0.8f;
+    [SerializeField] private float hitSmoothness = 10.0f;
 
     private int hits;
+    private float targetScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = new Vector3(
+            Mathf.Lerp(transform.localScale.x, targetScale, Time.deltaTime * hitSmoothness),
+            Mathf.Lerp(transform.localScale.y, targetScale, Time.deltaTime * hitSmoothness),
+            Mathf.Lerp(transform.localScale.z, targetScale, Time.deltaTime * hitSmoothness)
+        );
     }
-    public void Interact() {
+    public int Collect() {
         hits++;
+        transform.localScale = Vector3.one * hitScale;
         if (hits >= amountOfHits) {
-            Destroy(gameObject);
+            Destroy(gameObject, 1.0f);
+            targetScale = 0;
+            return resourceAmount;
         }
+        return 0;
     }
 }
