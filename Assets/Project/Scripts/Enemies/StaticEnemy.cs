ec8d76bc7503061f69a978070a6a99068fd6747c
Dejan Trajkovic <dtrajko@gmail.com>
2019-07-11 00:13:29 +0200

    Section 30: Targets - 272. Static Enemy

---
+++ Assets/Project/Scripts/Enemies/StaticEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEnemy : MonoBehaviour, IDamageable
+{
+    [SerializeField] private float health;
+    [SerializeField] private float hitSmoothness;
+
+    private float targetScale = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(
+            Mathf.Lerp(transform.localScale.x, targetScale, hitSmoothness * Time.deltaTime),
+            Mathf.Lerp(transform.localScale.y, targetScale, hitSmoothness * Time.deltaTime),
+            Mathf.Lerp(transform.localScale.z, targetScale, hitSmoothness * Time.deltaTime)
+        );
+    }
+    public int Damage(float amount)
+    {
+        if (health > 0) {
+            transform.localScale = Vector3.one * 0.9f;
+        }
+        health -= amount;
+
+        if (health <= 0) {
+            targetScale = 0;
+            Destroy(gameObject, 1.0f);
+        }
+        return 0;
+    }
+}
