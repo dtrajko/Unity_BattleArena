9a8a2c522300b497258c011588bd16c9c6eba02a
Dejan Trajkovic <dtrajko@gmail.com>
2019-07-10 02:24:13 +0200

    Section 29: Weapons - 269. Explosion

---
+++ Assets/Project/Scripts/Weapons/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public void Explode(float range, float damage) {
+        transform.GetChild(0).localScale = Vector3.one * range * 2;
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, range, transform.up);
+        foreach (RaycastHit hit in hits) {
+            Destroy(hit.transform.gameObject);
+        }
+        Destroy(gameObject, 0.5f);
+    }
+}
--- Assets/Project/Scripts/Weapons/Rocket.cs
+++ Assets/Project/Scripts/Weapons/Rocket.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     [SerializeField] private float speed = 15.0f;
+    [SerializeField] private float lifetime;
+    [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] private float explosionRange;
+    [SerializeField] private float explosionDamage;
 
     private Rigidbody rocketRigidbody;
+    private float timer;
 
     // Start is called before the first frame update
     void Start() {}
-
-    // Update is called once per frame
-    void Update() { }
 
     void Awake() {
         rocketRigidbody = GetComponent<Rigidbody>();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= lifetime) {
+            Explode();
+        }
+    }
+
     public void Shoot(Vector3 direction) {
         transform.forward = direction;
         rocketRigidbody.velocity = direction * speed;
+    }
+
+    public void OnTriggerEnter(Collider otherCollider) {
+        Explode();
+    }
+
+    private void Explode() {
+        GameObject explosion = Instantiate(explosionPrefab);
+        explosion.transform.position = transform.position;
+        explosion.GetComponent<Explosion>().Explode(explosionRange, explosionDamage);
+        Destroy(gameObject);
     }
 }
