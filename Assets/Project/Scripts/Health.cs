2ee9c8b0bb1fbba2ca21e1fdbd417829f65e2425
Dejan Trajkovic <dtrajko@gmail.com>
2019-07-17 06:49:17 +0200

    Section 31: Multiplayer - 292. Network Player Hit (1)

---
+++ Assets/Project/Scripts/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Obsolete]
+public class Health : NetworkBehaviour
+{
+    // Event
+    public delegate void HealthChangedHandler(float health);
+    public event HealthChangedHandler OnHealthChanged;
+
+    private const float defaultHealth = 100;
+
+    [SyncVar]
+    [System.Obsolete]
+    private float health = defaultHealth;
+
+    public void Damage(float amount) {
+        health -= amount;
+        if (health < 0) health = 0;
+
+        if (OnHealthChanged != null) { // If somebody subscribed to this event
+            OnHealthChanged(health);
+        }
+    }
+}
