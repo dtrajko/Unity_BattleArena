3bab52adfec7f4053388aa14b0f2b7569fe06a46
Dejan Trajkovic <dtrajko@gmail.com>
2019-07-17 02:46:16 +0200

    Section 31: Multiplayer - 290. Spawning Items

---
+++ Assets/Project/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Obsolete]
+public class Spawner : NetworkBehaviour
+{
+    [SerializeField] private GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject instance = Instantiate(prefab, transform.position, transform.rotation, transform.parent);
+        NetworkServer.Spawn(instance);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
