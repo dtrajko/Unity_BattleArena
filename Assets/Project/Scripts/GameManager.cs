e478364eca554e180f283199e428983974a07610
Dejan Trajkovic <dtrajko@gmail.com>
2019-09-27 20:10:34 +0200

    Implementing the mixamo SWAT model (2)

---
+++ Assets/Project/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("GameManager instance is NULL!");
+            }
+            return _instance;
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -78,4 +78,3 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         }
     }
 }
-
